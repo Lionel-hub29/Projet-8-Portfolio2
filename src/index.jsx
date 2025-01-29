220a6fe47733db97e413fd2152af7844fc3e627d
Lionel Bertrand <lionel.bertrand83@gmail.com>
2025-01-29 16:14:59 +0100

    correction index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Router from "./Router.jsx";
 
+// Composant pour gérer le scroll vers les ancres
+function ScrollToAnchor() {
+  const { hash } = useLocation();
+
+  useEffect(() => {
+    if (hash) {
+      const element = document.querySelector(hash);
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [hash]);
+
+  return null;
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    <ScrollToAnchor />
     <Router />
   </BrowserRouter>
 );
