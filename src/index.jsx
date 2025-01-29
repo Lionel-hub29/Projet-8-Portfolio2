ef8c89684ad0595f3109f421ca7a61e7a2f08795
Lionel Bertrand <lionel.bertrand83@gmail.com>
2025-01-29 17:27:03 +0100

    essai index

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,23 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, useLocation, useNavigate } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Router from "./Router.jsx";
 
 // Composant pour gérer le scroll vers les ancres
 function ScrollToAnchor() {
-  const { pathname, hash } = useLocation();
+  const { hash } = useLocation();
 
   useEffect(() => {
     if (hash) {
-      // Si le hash est présent, on scrolle vers l'élément correspondant
-      const element = document.querySelector(hash);
-      if (element) {
-        setTimeout(() => {
+      // Attendre que la page ait fini de se charger avant de scroller
+      setTimeout(() => {
+        const element = document.querySelector(hash);
+        if (element) {
           element.scrollIntoView({ behavior: "smooth" });
-        }, 100); // Délai pour garantir que l'élément est chargé avant de défiler
-      }
+        }
+      }, 100); // Délai pour garantir que l'élément est chargé avant de défiler
     }
   }, [hash]); // Ce useEffect se déclenche uniquement lorsque le hash change
 
